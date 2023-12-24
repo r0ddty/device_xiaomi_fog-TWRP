03993a9738631462367f13ee0a48760f89b596da
alternoegraha <noegrahachan@gmail.com>
2023-12-24 08:59:52 +0000

    fog: introduce vendorsetup.sh
    - now uses Sea kernel by @Asyanx as well
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Clone kernel tree (Sea kernel by Asyanx)
+git clone https://github.com/Asyanx/Sea_Kernel-Fog kernel/xiaomi/fog
+
+# Kernel fixups
+cd kernel/xiaomi/fog
+git reset --hard f1b696
+sed -i 's+R¹.+R0/Upstream+' arch/arm64/configs/vendor/fog-perf_defconfig # rebrand to fix kernel compilation issue during build
+cd ../../../
